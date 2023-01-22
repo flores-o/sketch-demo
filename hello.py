ff3f0ebc7772d1e88e4c14d46f2be81ec5c373e3
Tal Shiri <eiopaa@gmail.com>
2023-01-21 21:11:31 -0800

    stuff!

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 import os
+import json
 import requests
 os.environ["REPLICATE_API_TOKEN"]="5e8542a3f24574d987db5000c484a186cc2a3807"
 from flask import Flask, render_template
+from concurrent.futures import ThreadPoolExecutor
 import replicate
 from io import BytesIO
 app = Flask(__name__)
@@ -17,6 +19,7 @@ def script():
     return render_template("draw.html")
 from flask import request
 import base64
+from threading import Thread
 @app.route("/save", methods=["POST"])
 def save():
     header = "data:image/png;base64,"
@@ -29,11 +32,21 @@ def save():
 
     model = replicate.models.get("stability-ai/stable-diffusion-img2img")
     version = model.versions.get("15a3689ee13b0d2616e98820eca31d4c3abcd36672df6afce5cb6feb1d66087d")
-
     # https://replicate.com/stability-ai/stable-diffusion-img2img/versions/15a3689ee13b0d2616e98820eca31d4c3abcd36672df6afce5cb6feb1d66087d#input
+    subinputs = [
+    {
+        'prompt_strength': 0.4,
+        'prompt': "human figure with 2 arms and 2 legs on white background, 2d, drawing, children's book",
+    },
+    {
+        'prompt_strength': 0.8,
+        'prompt': "photorealistic human figure with 2 arms and 2 legs",
+    },
+
+    ]
     inputs = {
         # Input prompt
-        'prompt': "photorealistic human figure with 2 arms and 2 legs on white background, 2d",
+        'prompt': "human figure with 2 arms and 2 legs on white background, 2d, drawing, children's book",
 
         # The prompt NOT to guide the image generation. Ignored when not using
         # guidance
@@ -65,13 +78,21 @@ def save():
         # 'seed': ...,
     }
 
+    pool = ThreadPoolExecutor(2)
+    inputs = [{**inputs, **si} for si in subinputs]
+
     # https://replicate.com/stability-ai/stable-diffusion-img2img/versions/15a3689ee13b0d2616e98820eca31d4c3abcd36672df6afce5cb6feb1d66087d#output-schema
-    output = version.predict(**inputs)
-    url = output[0]
-    print("Got url", url)
+    outputs = list(pool.map(lambda inp: version.predict(**inp), inputs))
+    urls = [x[0] for x in outputs]
+    print("Got url", urls)
+    urls = list(pool.map(make_anim, urls))
+    print("Got movie url", urls)
+    # print(url)
+    return json.dumps(urls)
+
+def make_anim(url):
+    print("Downloading", url)
     image_resp = requests.get(url)
     open("image.png", "wb").write(image_resp.content)
     url = animatify(image_resp.content, ANIMATIONS[0])
-    print(url)
     return url
-
